83a2c8e6b32abd1dd37555e180a1810073230661
takurofukuta <mojuraspax@icloud.com>
2021-02-24 14:41:05 +0900

    main.rbのwhile の条件式を修正

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ require "./monster"
 require "./brave"
 require "./remaining_hp"
 
-monster = Monster.new(name: 'アークデーモン', hp: 210, offense: 140, defense: 80)
-# monster = Monster.new(name: "シドー", hp: 250, offense: 255, defense: 255)
+# monster = Monster.new(name: 'アークデーモン', hp: 210, offense: 140, defense: 80)
+
+monster = Monster.new(name: "シドー", hp: 250, offense: 255, defense: 255)
 brave = Brave.new(name: "ゆうしゃ", hp: 238, offense: 203, defense: 129)
 
-while true
-  brave.attack(monster) if monster.hp > 0
-  monster.attack(brave) if brave.hp > 0
+while monster.hp > 0 && brave.hp > 0
+  brave.attack(monster) if brave.hp > 0
+  monster.attack(brave) if monster.hp > 0
   RemainingHp.new(brave,monster)
-  break if monster.hp == 0 || brave.hp == 0
 end
